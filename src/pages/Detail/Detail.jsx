03db35e59b706f1b3c63d4c79841c3304e358727
Nguyễn Hoàng Phúc <nguyenhoangphuc@MacBook-Pro-cua-Nguyen.local>
2022-08-03 18:51:10 +0700

    fix

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,68 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
-import Lesson from '../../Components/Lesson/Lesson';
-import { useHistory, useNavigate } from 'react-router';
-import {AiTwotoneStar} from "react-icons/ai"
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import Lesson from "../../Components/Lesson/Lesson";
+import { useHistory, useNavigate } from "react-router";
+import { AiTwotoneStar } from "react-icons/ai";
+import { useSelector } from "react-redux";
 import ReactStars from "react-rating-stars-component";
-import Comment from './Comment';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import Comment from "./Comment";
+import { FaStar } from "react-icons/fa";
 const Detail = (props) => {
-    const handleChange = (newRating) => {
-        console.log(newRating)
+  const handleChange = (newRating) => {
+    console.log(newRating);
+  };
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
+  useEffect(() => {
+    function resizewindow () {
+        setWindowSize(window.innerWidth)
     }
-  
-    const navigate = useNavigate()
-    const check = useSelector(state => state.UserReducer.check)
-    /* useEffect(() => {
+    window.addEventListener("resize", resizewindow)
+  })
+
+  /* const navigate = useNavigate();
+  const check = useSelector((state) => state.UserReducer.check);
+  useEffect(() => {
         if(check !== 0) {
             alert("Vui long dang nhap vao tai khoan!!!")
             navigate("/signin")
             console.log(check)
         }
     }, []) */
- 
-    const src = 'https://hd.1080phim.com/20220729/26193_a0876f48/index.m3u8'
 
-    return (
-        <div className='detail'>
-            <div className="detail__cover">
+  const src = "https://hd.1080phim.com/20220729/26193_a0876f48/index.m3u8";
+  const secondExample = {
+    size: 17,
+    count: 5,
+    activeColor: "yellow",
+    value: 3,
+    a11y: true,
+    emptyIcon: <i className="fa-solid fa-star"></i>,
+    filledIcon: (
+      <i style={{ color: "yellow" }} className="fa-solid fa-star"></i>
+    ),
+  };
+  return (
+    <div className="detail">
+      <div className="detail__cover">
         <div className="detail__left">
-            <div className="video">
-            <ReactPlayer 
-            width="100%"
-            height="100%"
-            controls={true}
-            url={src}
-      />
+          <div className="video">
+            <ReactPlayer width="100%" height="100%" controls={true} url={src} />
             <div className="video__title">
-                    <h2>Introduction Design Graphic</h2>
-                </div>
-                <ReactStars value={3} count={5} size={20} onChange={handleChange}/>
+              <h2>Introduction Design Graphic</h2>
             </div>
-            
-          {window.innerWidth > 750 &&  <Comment/>}
+            <div className="rating" style={{ marginTop: 10 }}>
+              <ReactStars {...secondExample} onChange={handleChange} />
+            </div>
+          </div>
+
+          {windowSize > 750 && <Comment />}
         </div>
         <div className="detail__right">
-            <div className="detail__right__lessons">
-               <ul>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>
-                <li><Lesson/> </li>                
-               </ul>
-            </div>
+          <div className="detail__right__lessons">
+            <ul>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+              <li>
+                <Lesson />{" "}
+              </li>
+            </ul>
+          </div>
         </div>
-        {window.innerWidth <= 750 && <Comment/>}
-
-            </div>
-
-        </div>
-    );
+        {windowSize <= 750 && <Comment />}
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
